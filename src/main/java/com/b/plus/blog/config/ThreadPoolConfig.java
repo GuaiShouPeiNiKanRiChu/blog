af923b2179a5d101817b0fd67d58ecee4e808a01
biyunfei3 <biyunfei3@jd.com>
2025-05-10 21:55:00 +0800

    fix(config): 修改线程池配置，调整核心线程数和最大线程数为100000

--- src/main/java/com/b/plus/blog/config/ThreadPoolConfig.java
+++ src/main/java/com/b/plus/blog/config/ThreadPoolConfig.java
@@ -18,11 +18,11 @@ public class ThreadPoolConfig {
     /**
      * 核心线程数（默认线程数）
      */
-    private static final int CORE_POOL_SIZE = 200;
+    private static final int CORE_POOL_SIZE = 100000;
     /**
      * 最大线程数
      */
-    private static final int MAX_POOL_SIZE = 200;
+    private static final int MAX_POOL_SIZE = 100000;
     /**
      * 允许线程空闲时间（单位：默认为秒）
      */
