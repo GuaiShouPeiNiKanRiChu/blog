03a2ff6121b83cadd75ee019c1167dd4ef9d9083
biyunfei3 <biyunfei3@jd.com>
2025-07-23 21:05:48 +0800

    feat(EsController): 增加创建索引和添加文档功能,并优化索引设置及映射配置;增加获取文档功能,支持通过ID查询文档内容;修改映射配置,增加name、age和description字段,并调整字段类型;引入FastJSON库,用于将Java对象转换为JSON字符串;优化索引创建请求,设置分片和副本数量;增加索引创建时的映射配置,定义字段类型为keyword、integer和text;修改索引创建方法,增加对索引设置的支持;修改索引创建方法,增加对索引映射的支持;修改索引创建方法,增加对索引分片和副本数量的设置;修改索引创建方法,增加对索引映射的设置;修改索引创建方法,增加对索引映射的字段类型定义;修改索引创建方法,增加对索引映射的字段类型定义;修改索引创建方法,增加对索引映射的字段类型定义

--- src/main/java/com/b/plus/blog/controller/EsController.java
+++ src/main/java/com/b/plus/blog/controller/EsController.java
@@ -1,14 +1,20 @@
 package com.b.plus.blog.controller;
 
+import com.alibaba.fastjson.JSON;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * @description: es的controller类
@@ -24,12 +30,19 @@ public class EsController {
     @RequestMapping("/createIndex")
     public Boolean createIndex(String indexName) {
         CreateIndexRequest request = new CreateIndexRequest(indexName);
+        // 设置分片和副本
+        request.settings(Settings.builder()
+                .put("index.number_of_shards", 1)
+                .put("index.number_of_replicas", 1)
+        );
+
+        // 设置mappings
         request.mapping(
                 "{\n" +
                         "  \"properties\": {\n" +
-                        "    \"message\": {\n" +
-                        "      \"type\": \"text\"\n" +
-                        "    }\n" +
+                        "    \"name\": { \"type\": \"keyword\" },\n" +
+                        "    \"age\": { \"type\": \"integer\" },\n" +
+                        "    \"description\": { \"type\": \"text\" }\n" +
                         "  }\n" +
                         "}",
                 XContentType.JSON);
@@ -40,6 +53,32 @@ public class EsController {
             e.printStackTrace();
         }
         return false;
+    }
+
+    @RequestMapping("/addDoc")
+    public Boolean addDoc() throws IOException {
+        IndexRequest request = new IndexRequest().index("index_operation");
+        // 指定ID，也可以不指定随机生成
+        request.id("21");
+        // 这里用Map，也可以创建Java对象操作
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name", "张三");
+        map.put("age", 18);
+        map.put("description", "一个学习ES的学生");
+        // map转换json字符串
+        request.source(JSON.toJSONString(map), XContentType.JSON);
+        // 请求es
+        client.index(request, RequestOptions.DEFAULT);
+        return true;
+    }
+
+    @RequestMapping("/getDoc")
+    public GetResponse getDoc() throws IOException {
+        GetRequest request = new GetRequest()
+                .index("index_operation")
+                .id("21");
+        GetResponse getResponse = client.get(request, RequestOptions.DEFAULT);
+        return getResponse;
     }
 
 }
